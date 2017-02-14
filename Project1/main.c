c7db2d88a7a46d52bd2ec9452aaef9a28a7385ce
Raunaq Arora <raunaqarora@raunaqs-mbp.mobile.asu.edu>
2017-02-14 16:48:51 -0700

    Initial Commit

---
+++ Project1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Project1
+//
+//  Created by Raunaq Arora on 2/14/17.
+//  Copyright © 2017 Raunaq Arora. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
